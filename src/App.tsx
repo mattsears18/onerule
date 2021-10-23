3e13a147f7fb670f8a6113011637e49f5640f0e4
Matt Sears <mattsears18@gmail.com>
2021-10-23 19:26:51 -0400

    rename query component, prep for subscription
    rename query component, prep for subscription

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-
 import OnlineUsers from './components/online_users';
+import OnlineUsersQuery from './components/online_users_query';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <header className="App-header">
         <h2>rulemaker</h2>
       </header>
+      <OnlineUsersQuery />
       <OnlineUsers />
     </div>
   );
