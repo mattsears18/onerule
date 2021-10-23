115d564fc504c7520de09d72130560836407e1b2
Matt Sears <mattsears18@gmail.com>
2021-10-23 19:22:44 -0400

    query working

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,17 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
 import './App.css';
 
-const EXCHANGE_RATES = gql`
-  query GetExchangeRates {
-    rates(currency: "USD") {
-      currency
-      rate
-    }
-  }
-`;
-
-function ExchangeRates() {
-  const { loading, error, data } = useQuery(EXCHANGE_RATES);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  return data.rates.map(({ currency, rate }: { currency: any; rate: any }) => (
-    <div key={currency}>
-      <p>
-        {currency}: {rate}
-      </p>
-    </div>
-  ));
-}
+import OnlineUsers from './components/online_users';
 
 function App() {
   return (
-    <div>
-      <h2>My first Apollo app 🚀</h2>
-      <ExchangeRates />
+    <div className="App">
+      <header className="App-header">
+        <h2>rulemaker</h2>
+      </header>
+      <OnlineUsers />
     </div>
   );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
---
+++ src/components/online_users.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import './online_users.css';
+
+const ONLINE_USERS = gql`
+  query GetOnlineUsers {
+    online_users_aggregate {
+      aggregate {
+        count
+      }
+    }
+    online_users {
+      name
+      last_seen
+    }
+  }
+`;
+
+const OnlineUsers: React.FC = () => {
+  const { loading, error, data } = useQuery(ONLINE_USERS);
+
+  console.log(data);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    console.error(error);
+    return <p>Error :(</p>;
+  }
+
+  return (
+    <div className={'online-users'}>
+      <h2>Online Users ({data.online_users_aggregate.aggregate.count})</h2>
+      <div>
+        {data.online_users.map((user: { name: string }) => {
+          console.log(user);
+          return <div>{user.name}</div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default OnlineUsers;
